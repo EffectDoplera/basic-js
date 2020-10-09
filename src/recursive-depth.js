a01b2418f77ec0011362bb4ee4c5b458ec5462f5
Mikhail Danshin <effectdopler0@gmail.com>
2020-10-09 11:39:29 +0300

    ref: ref som settings

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+// module.exports = 
+class DepthCalculator {
+  constructor() {
+    this.depth = 0;
+    this.max = 0;
+  }
+  calculateDepth(arr) {
+    this.depth += 1
+
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        this.calculateDepth(arr[i])
+      }
+    }
+
+    if (this.depth > this.max) {
+      this.max = this.depth
+    }
+    this.depth -= 1
+    
+    if (this.depth === 0) {
+      return this.max
+    }
   }
 };
+
+const calc = new DepthCalculator().calculateDepth([1, 2, 3, 4, 5, [1, []]])
+console.log(calc);
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('Argument is not Array')
+
+  const res = [...arr].filter((item) => !isNaN(parseInt(item)))
+
+  for (let i = 0; i <= arr.length; i++) {
+    if (res[i] === '--double-next') {
+      res[i] = undefined
+      res[i + 1] *= 2
+    }
+    
+    if (res[i] === '--double-prev') {
+      res[i] = undefined
+      res[i - 1] *= 2
+    }
+  
+    if (res[i] === '--discard-next') {
+      res[i] = undefined
+      res[i + 1] = undefined
+    }
+  
+    if (res[i] === '--discard-prev') {
+      res[i] = undefined
+      res[i - 1] = undefined
+    }
+  }
+
+  return res.filter((item) => item !== undefined)
 };
